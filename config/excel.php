98ee7e589fe8377a118af2ccec4990b0fef14bf6
emrfa <muhammademirfaisa@gmail.com>
2025-12-01 20:38:10 +0700

    Fix Excel Permission

--- config/excel.php
+++ config/excel.php
@@ -95,16 +95,7 @@ return [
 
     'temporary_files' => [
         'local_path' => storage_path('app'),
-        'local_permissions' => [
-            'file' => [
-                'public' => 0644,
-                'private' => 0600,
-            ],
-            'dir' => [
-                'public' => 0755,
-                'private' => 0700,
-            ],
-        ],
+        'local_permissions' => 0755,
         'remote_disk' => null,
         'remote_prefix' => null,
         'force_resync_remote' => null,
